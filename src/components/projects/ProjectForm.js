81475d45adeb20665d2826f9b4a215736f627e17
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-06-27 10:21:35 -0300

    API local criada, e conxão estabelicida.

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,9 +1,31 @@
+import React from 'react';
 import styles from './ProjectForm.module.css';
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 const ProjectForm = ({ btnText }) => {
+
+  // trecho de código que puxa os dados da API usando 'useEffect', e usando 'useState' para rendenrizar as opções a API diremente na página de novos projetos.
+  // para iniciar o projeto e carregar os dados do 'db.json', execute 'npm run backend' no terminal
+
+  const [ categories, setCategories ] = React.useState([]);
+  const url = "http://localhost:5000/categories";
+
+  React.useEffect(() => {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setCategories(data);
+    })
+    .catch(err => console.log(err));
+  }, []);
+
   return (
     <>
       <form className={ styles.form }>
@@ -19,7 +41,12 @@ const ProjectForm = ({ btnText }) => {
         name="budget"
         placeholder="Insira o orçamento total:"
         />
-        <Select name="category_id" text="Selecione a categoria"/>
+        <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={ categories }
+          
+        />
         <SubmitButton text={ btnText }/>
       </form>
     </>
