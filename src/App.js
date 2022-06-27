8d16e62c443e8d888240bb4800e84e4703f3afcb
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-06-27 06:25:22 -0300

    Estrutura do Projeto criada.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './layout/Container';
+
 
 const App = () => {
   return (
-    <></>
+    <BrowserRouter> 
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </ul>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path= "/" element={<Home/>}/> 
+          <Route path= "/Company" element={<Company/>}/> 
+          <Route path= "/Contact" element={<Contact/>}/> 
+          <Route path= "/NewProject" element={<NewProject/>}/>    
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </BrowserRouter>
   )
 }
 
