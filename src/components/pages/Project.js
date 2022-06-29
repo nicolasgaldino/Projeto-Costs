0a97e8c439349009ce6279e12c795816a097e28a
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-06-29 13:36:18 -0300

    Área de serviços criada.

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,12 +3,17 @@ import styles from './Project.module.css';
 import { useParams } from 'react-router';
 import Loading from '../layout/Loading';
 import Container from '../layout/Container';
+import ProjectForm from '../projects/ProjectForm';
+import Message from '../layout/Message';
 
 const Project = () => {
 
   const { id } = useParams();
   const [ project, setProject ] = React.useState([]);
   const [ showProjectForm, setShowProjectForm ] = React.useState(false);
+  const [ showServiceForm, setShowServiceForm ] = React.useState(false);
+  const [ message, setMessage ] = React.useState();
+  const [ messageType, setMessageType ] = React.useState();
 
   React.useEffect(() => {
     const url = `http://localhost:5000/projects/${id}`;
@@ -26,8 +31,40 @@ const Project = () => {
     .catch((err) => console.log(err));
   }, [id])
 
+  function editPost(project) {
+    setMessage('')
+
+    if(project.budget < project.cost) {
+      setMessage('O orçamento não pode ser menor que o custo do projeto.')
+      setMessageType('error')
+      return false
+    }
+
+    const url = `http://localhost:5000/projects/${project.id}`;
+
+    fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setProject(data);
+      setShowProjectForm(false)
+      setMessage('Projeto atualizado.')
+      setMessageType('success')
+    })
+    .catch((err) => console.log(err));
+  }
+
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
+  }
+
+  function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm);
   }
 
   return (
@@ -35,6 +72,8 @@ const Project = () => {
       {project.name ? (
         <div className={ styles.projectDetails }>
           <Container customClass="column">
+            {message && 
+              <Message type={ messageType } msg={ message } />}
             <div className={ styles.detailsContainer }>
               <h1>Projeto: { project.name }</h1>
               <button
@@ -57,10 +96,30 @@ const Project = () => {
                 </div>
               ) : (
                 <div className={ styles.projectInfo }>
-                  <p>Detalhes do projeto</p>
+                  <ProjectForm
+                  btnText="Concluir edição"
+                  projectData={ project }
+                  handleSubmit={ editPost }
+                  />
                 </div>
                 )}
             </div>
+            <div className={ styles.serviceFormContainer }>
+              <h2>Adicione um serviço:</h2>
+              <button
+                className={ styles.btn }
+                onClick={ toggleServiceForm }
+              >
+                {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+              </button>
+              <div className={ styles.projectInfo }>
+                {showServiceForm && <div>Formulário do serviço</div>}
+              </div>
+            </div>
+            <h2>Serviços</h2>
+            <Container customClass="start">
+              <p>Itens de serviços</p>
+            </Container>
           </Container>
         </div>
       ) : (
